79cf200d663866c86481800b910b5a7e6c3c9153
phil <sakalfil@fit.cvut.cz>
2016-11-25 13:58:49 +0100

    refactoring

--- README.md
+++ README.md
@@ -4,15 +4,16 @@ Picture similarity retrieval with SURF
 =======
 ## This is semestral project for subject MI-VMM@FIT-CTU
 It contains 2 main parts:
-* Interest points finder (folder ipfinder)
+* Interest points finder (folder ipmatching/public/ipfinder)
+ * jopensurf library (https://code.google.com/archive/p/jopensurf)
 * IP matching & Web app (folder ipmatching)
  * Ruby on Rails app
  * This app will call ipfinder on every uploaded picture in order to get Interest Points.
  
-## How to run
+## Usage
 1. Interest point finder
 ```sh
-$ java -jar "ipfinder/dist/semestralka.jar" file1 file2 ...
+$ java -jar "ipmatching/public/ipfinder/dist/semestralka.jar" file1 file2 ...
 ```
 
 Example output:
@@ -24,6 +25,7 @@ Surf Interest Points Finder:
 462.39285 52.582752
 354.75287 53.189285
 448.75555 59.47033
+...
 ```
 
 2. Web app
@@ -33,3 +35,5 @@ Surf Interest Points Finder:
 $ cd ipmatching
 $ rails server
 ```
+And show it in your browser (default address is localhost:3000).
+
--- ipmatching/Matching/app/models/picture.rb
+++ ipmatching/Matching/app/models/picture.rb
@@ -6,31 +6,33 @@ class Picture < ActiveRecord::Base
   after_save :analyze_ips
 
   def analyze_ips
-    output = `cd ~/Dokumenty/mi-vmm/MI-VMM/ipfinder/dist; java -jar ./semestralka.jar #{image.path}`
+    output = `cd #{Rails.root.join("public", "ipfinder", "dist")}; java -jar ./semestralka.jar #{image.path}`
+    raise "#{output}"
     output.split("\n")[1..-1].each do |ips|
       xy = ips.split(' ')
       interest_points.create(x: xy[0].to_f, y: xy[1].to_f)
     end
   end
 
-  def match(other_picture = Picture.second, prah = 0.5)
+  def partial_match(my_point, other_points)
+    partial_result = []
+    other_points.each do |oip|
+      partial_result << Math.sqrt(((my_point.x - oip.x) * (my_point.x - oip.x))+((my_point.y - oip.y) * (my_point.y - oip.y)))
+    end
+    partial_result.sort!
+    [partial_result[0], partial_result[1]]
+  end
+
+  def match(other_picture = Picture.second, threshold = 0.5)
     result = { cnt: 0, perc: 0.0 }
     interest_points.each do |ip|
-      partial_result = {}
-      other_picture.interest_points.each do |oip|
-        partial_result[oip] = Math.sqrt(((ip.x - oip.x) * (ip.x - oip.x))+((ip.y - oip.y) * (ip.y - oip.y)))
-      end
-      sorted = partial_result.sort_by { |k, v| v }
-      if sorted.first[1] / sorted.second[1] >= prah || sorted.first[1] == 0 # Najde 1 identicky bod
+      first, second = partial_match(ip, other_picture.interest_points)
+      if first / second <= threshold # || partial_result.first[1] == 0 # Najde 1 identicky bod
         result[:cnt] += 1
       end
     end
     # result[:perc] = (result[:cnt] + 0.0) / interest_points.count
-    if interest_points.count >= other_picture.interest_points.count
-      result[:perc] = (result[:cnt] + 0.0) / interest_points.count
-    else
-      result[:perc] = (result[:cnt] + 0.0) / other_picture.interest_points.count
-    end
+    result[:perc] = (result[:cnt] + 0.0) / [interest_points.count, other_picture.interest_points.count].max
     result
   end
 end
